3b7442f9a2cc6903b4922c180b24fa6eafad2f7f
goose <40964328+TDLPoof@users.noreply.github.com>
2024-12-16 11:52:48 -0600

    Added Vector2, PhysicsBody, Collider
    Vector2 is usable for other systems
    PhysicsBody is currently in development (requires a GameManager to check for all Colliders in scene for collision update purposes)
    Collider is done, pending bugfixes

---
+++ goose-fight/src/main/java/net/parkwayschools/phys/Vector2.java
@@ -0,0 +1,40 @@
+public class Vector2
+{
+    public static const zero = new Vector2(0, 0)
+
+    public double x, y;
+
+    // constructors
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Vector2(double x) {
+        this.x = x;
+        this.y = x;
+    }
+
+    @Override public String toString() { return "Vector2(" + x + ", " + y + ")"; }
+
+    @Override
+    public boolean equals(Vector2 other) {
+        double tolerance = 1e-6;
+        return Math.abs(x - other.x) < tolerance && Math.abs(y - other.y) < tolerance;
+    }
+
+
+    public double getAngle() { return Math.atan2(y, x); }
+    public double getLength() { return Math.sqrt(x * x + y * y); }
+
+    public void normalize() { x /= getLength(); y /= getLength(); }
+
+    public double distanceSq(Vector2 other) { return (x-other.x) * (x-other.x) + (y-other.y) * (y-other.y); }
+    public double distance(Vector2 other) { return Math.sqrt(distanceSq(other)); }
+
+    public void add(Vector2 other) { x += other.x; y += other.y; }
+    public void sub(Vector2 other) { x -= other.x; y -= other.y; }
+    public void mult(Vector2 other) { x *= other.x; y *= other.y; }
+    public void div(Vector2 other) { x /= other.x; y /= other.y; }
+    public void mult(double scale) { x *= scale; y *= scale; }
+    public void div(double scale) { x /= scale; y /= scale; }
+}
