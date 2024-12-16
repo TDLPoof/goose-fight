730fa3c8de519ce458dd2cd12a82c475d840d404
Luminoso-256 <63971285+Luminoso-256@users.noreply.github.com>
2024-12-16 12:08:32 -0600

    Basic scaffold

--- goose-fight/src/main/java/net/parkwayschools/Main.java
+++ goose-fight/src/main/java/net/parkwayschools/Main.java
@@ -1,7 +1,20 @@
 package net.parkwayschools;
 
+import net.parkwayschools.gfx.GfxManager;
+import net.parkwayschools.util.Log;
+
 public class Main {
+    static Log log = new Log("main");
+    static GfxManager _gfx = new GfxManager();
+    static Thread _gfxThread;
+
     public static void main(String[] args) {
-        System.out.println("Epic cool goose game lesgo");
+        log.inf("Starting GFX thread");
+        _gfxThread = new Thread(_gfx);
+        _gfxThread.start();
+
+        while (true){
+            //hold the main thread open
+        }
     }
 }
