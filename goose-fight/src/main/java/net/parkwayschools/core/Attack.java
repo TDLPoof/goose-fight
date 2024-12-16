e0e55b34b48f2e41d87622482a022451bbb5c6ef
Umar Rafiq <rafiqu1758@parkwayschools.net>
2024-12-16 10:52:54 -0600

    started writing the Attack.java

--- goose-fight/src/main/java/net/parkwayschools/core/Attack.java
+++ goose-fight/src/main/java/net/parkwayschools/core/Attack.java
@@ -1,1 +1,11 @@
+package net.parkwayschools.core;
+
 //umar will create an incredible piece of code here shortly.
+public class Attack {
+    public double damage;
+    public double frames;
+    private String test;
+
+
+
+}
