25281a905b9f06fade325e369c3c0325a1cb9299
Luminoso-256 <63971285+Luminoso-256@users.noreply.github.com>
2024-12-17 09:31:44 -0600

    Basic scaffold pt 2

--- goose-fight/src/main/java/net/parkwayschools/gfx/GfxManager.java
+++ goose-fight/src/main/java/net/parkwayschools/gfx/GfxManager.java
@@ -2,6 +2,7 @@ package net.parkwayschools.gfx;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /*
 * Implements the graphics and render thread
@@ -9,21 +10,29 @@ import java.util.ArrayList;
 public class GfxManager implements Runnable{
 
     JFrame _frame;
+    RenderPanel _rp;
     ArrayList<RenderObj> _currentRQ;
+    HashMap<String,Spritesheet> _sheets;
 
     public GfxManager(){
         _currentRQ = new ArrayList<>();
     }
 
-    void submitRenderQueue(ArrayList<RenderObj> ros){
+    void initSpritesheets(){
+
+    }
+
+    public void submitRenderQueue(ArrayList<RenderObj> ros){
 
     }
 
     @Override
     public void run() {
+        _rp = new RenderPanel();
         _frame = new JFrame("Cool Goose Fighting Game:tm:");
         _frame.setSize(800,500);
         _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        _frame.add(_rp);
         _frame.setVisible(true);
         while (true){
             for (RenderObj ro : _currentRQ){
