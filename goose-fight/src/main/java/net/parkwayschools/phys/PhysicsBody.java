dab2d75913769408986e186b76a1240a89d0126c
goose <40964328+TDLPoof@users.noreply.github.com>
2025-01-10 13:31:14 -0600

    Add crouching and uncrouching to physics bodies
    Use this one! it adjusts position to keep the character grounded.

--- goose-fight/src/main/java/net/parkwayschools/phys/PhysicsBody.java
+++ goose-fight/src/main/java/net/parkwayschools/phys/PhysicsBody.java
@@ -1,28 +1,77 @@
 import java.util.ArrayList;
 
-public abstract class PhysicsBody
+public class PhysicsBody
 {
-    public double mass;
-    public double drag;
+    public static final Vector2 GRAVITY = new Vector2(0, 0.98);
+
+    public double mass, restitution;
     public Vector2 position;
     public Vector2 velocity, acceleration;
-    public Vector2 gravity;
+    public Vector2 drag;
+    public Vector2 groundedDrag;
+    public int jumps = 2;
+    public int walljumps = 2;
+
+    public boolean grounded = false, walled = false;
 
     public Collider collider;
+    public ArrayList<Collider> collisionObjects;
 
-    public void update() {
-        position += velocity;
-        collider.position = position;
-        for (Collider c : GameManager.GetCollidersInGame()) {
-            if (collider == c) continue;
-            if (c.isHurtbox) continue;
-            if (collider.intersects(c)) {
-                position -= velocity;
-                collider.position = position;
-            }
-        }
-        velocity += acceleration + gravity;
+    public PhysicsBody(double x, double y, double width, double height, double m, Vector2 d, double r, String n) {
+        collisionObjects = new ArrayList<>();
+        position = new Vector2(x, y);
+        collider = new Collider(x, y, width, height, n);
+        velocity = Vector2.zero;
+        acceleration = Vector2.zero;
+        mass = m;
+        drag = d;
+        groundedDrag = new Vector2(Math.sqrt(1 - (d.x - 1) * (d.x - 1)), d.y);
+        restitution = r;
     }
 
-    public void addForce(Vector2 force) {acceleration += force.div(mass); }
+    public void update() {
+        position.add(velocity);
+        collider.position = new Vector2(position.x, position.y);
+        for (Collider c : collisionObjects) {
+            if (collider == c) continue;
+            if (c.isHurtbox) continue;
+            grounded = false;
+            walled = false;
+
+            if (collider.intersects(c)) {
+                if (collider.goodHoriIntersects(c)) {
+                    position.x -= velocity.x;
+                    collider.position = new Vector2(position.x, position.y);
+                    velocity.x *= -restitution;
+                    if (!collider.name.equals("tBox")) System.out.println("Horizontal Intersection Detected [" + collider.name + " | " + c.name + "]");
+                    walled = true;
+                }
+                if (collider.goodVertIntersects(c)) {
+                    position.y -= velocity.y;
+                    collider.position = new Vector2(position.x, position.y);
+                    grounded = true;
+                    jumps = 2;
+                    walljumps = 2;
+                    velocity.y *= -restitution;
+                    if (!collider.name.equals("tBox")) System.out.println("Vertical Intersection Detected [" + collider.name + " | " + c.name + "]");
+                }
+            }
+        }
+        velocity.add(acceleration);
+        velocity.add(GRAVITY);
+        if (jumps >= 2) velocity.mult(new Vector2(1 - groundedDrag.x, 1 - groundedDrag.y));
+        else velocity.mult(new Vector2(1 - drag.x, 1 - drag.y));
+    }
+
+    public void addForce(Vector2 force) {velocity.add(new Vector2(force.x / mass, force.y / mass)); }
+
+    public void crouch() {
+        collider.crouch();
+        position = new Vector2(position.x, position.y + collider.size.y);
+    }
+
+    public void uncrouch() {
+        position = new Vector2(position.x, position.y - collider.size.y);
+        collider.uncrouch();
+    }
 }
